4d11f3c77e24ee9844ec7083f94f0d6654edf9a9
haneeoh <honey9473@gmail.com>
2025-03-11 15:35:29 +0900

    batis

---
+++ src/main/java/org/example/springmybatisgradle/SpringMybatisGradleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springmybatisgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springmybatisgradle/SpringMybatisGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmybatisgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
