668b6ce298c657970d69434556955e90b360bc70
haneeoh <honey9473@gmail.com>
2025-03-14 11:57:14 +0900

    0313

--- src/main/java/naver/storage/NcpObjectStorageService.java
+++ src/main/java/naver/storage/NcpObjectStorageService.java
@@ -1,5 +1,13 @@
 package naver.storage;
 
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -8,19 +16,13 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
 
 @Service
 public class NcpObjectStorageService implements ObjectStorageService {
     AmazonS3 s3;
 
     public NcpObjectStorageService(NaverConfig naverConfig) {
+
         System.out.println("NcpObjectStorageService 생성");
         s3 = AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
@@ -32,38 +34,47 @@ public class NcpObjectStorageService implements ObjectStorageService {
 
     @Override
     public String uploadFile(String bucketName, String directoryPath, MultipartFile file) {
-        System.out.println("uploadFile=" + file.getOriginalFilename());
+        System.out.println("uploadFile="+file.getOriginalFilename());
 
         if (file.isEmpty()) {
             return null;
         }
+
         try (InputStream fileIn = file.getInputStream()) {
             SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmm_");
-            String filename = sdf.format(new Date())+UUID.randomUUID().toString().substring(0,10)
+            String filename =sdf.format(new Date())+UUID.randomUUID().toString().substring(0,10)
                     +"."+file.getOriginalFilename().split("\\.")[1];
+
             ObjectMetadata objectMetadata = new ObjectMetadata();
             objectMetadata.setContentType(file.getContentType());
 
-            PutObjectRequest objectRequest = new PutObjectRequest
-                    (bucketName,directoryPath+"/"+filename, fileIn, objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead);
+            PutObjectRequest objectRequest = new PutObjectRequest(
+                    bucketName,
+                    directoryPath +"/"+ filename,
+                    fileIn,
+                    objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead);
+
             s3.putObject(objectRequest);
-//            return s3.getUrl(bucketName,directoryPath+filename).toString();
+
+            //return s3.getUrl(bucketName, directoryPath + filename).toString();
             return filename;
-        } catch (Exception e){
-            throw new RuntimeException("파일 업로드 오류",e);
+
+        } catch (Exception e) {
+            throw new RuntimeException("파일 업로드 오류", e);
         }
     }
 
     @Override
     public void deleteFile(String bucketName, String directoryPath, String fileName) {
-        String path = directoryPath+"/"+fileName;
-        //해당 버킷에 파일이 존재하면 true 반환
-        boolean isfind=s3.doesObjectExist(bucketName, path);
-        //존재할 경우 삭제
-        if(isfind){
-            s3.deleteObject(bucketName, path);
-            System.out.println(path+"삭제완료");
+        // TODO Auto-generated method stub
+        String path=directoryPath+"/"+fileName;
+        //해당 버킷에 파일이 존재하면 true반환
+        boolean isfind=s3.doesObjectExist(bucketName,path);
+        //존재할경우 삭제
+        if(isfind) {
+            s3.deleteObject(bucketName,path);
+            System.out.println(path+":삭제완료");
         }
     }
+
 }
